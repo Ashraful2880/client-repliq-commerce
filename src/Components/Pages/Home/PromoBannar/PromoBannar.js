61ef71cb9143facca5cc99d48425dc9cf3328d5c
Ashraful Islam <ashraful.islam@aamartaka.com>
2023-05-01 21:46:41 +0600

    Promo Bannar Design Done

--- src/Components/Pages/Home/PromoBannar/PromoBannar.js
+++ src/Components/Pages/Home/PromoBannar/PromoBannar.js
@@ -1,5 +1,4 @@
-import React from "react";
-import promoBannar from "../../../../Assets/Images/Others/hot-deal.png";
+import React, { useState, useEffect, useRef } from "react";
 import care1 from "../../../../Assets/Images/Others/care-1.png";
 import care2 from "../../../../Assets/Images/Others/care-2.png";
 import care3 from "../../../../Assets/Images/Others/care-3.png";
@@ -7,53 +6,119 @@ import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs";
 
 const PromoBannar = () => {
+  const [timerDays, setTimerDays] = useState("0000");
+  const [timerHours, setTimerHours] = useState("00");
+  const [timerMinutes, setTimerMinutes] = useState("00");
+  const [timerSeconds, setTimerSeconds] = useState("00");
+  const interval = useRef();
+
+  const startTimer = () => {
+    const countDownDate = new Date("December 31, 2024").getTime();
+    interval.current = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countDownDate - now;
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        clearInterval(interval.current);
+      } else {
+        setTimerDays(days.toString().padStart(2, "0"));
+        setTimerHours(hours.toString().padStart(2, "0"));
+        setTimerMinutes(minutes.toString().padStart(2, "0"));
+        setTimerSeconds(seconds.toString().padStart(2, "0"));
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    };
+  }, []);
+
   return (
-    <div className="pb-12 lg:px-0 px-4 relative">
-      <div>
-        <Link to="/shopCategory/mask">
-          <img
-            src={promoBannar}
-            alt="Promo_Bannar_image"
-            className="w-full pb-12"
-          />
-        </Link>
-        <div className="absolute right-40 top-52 uppercase lg:block hidden text-left">
-          <h3 className="text-3xl text-[#2563eb] font-bold">
-            Sales Offer & Discount Only
-          </h3>
-          <h1 className="text-6xl upparcase font-bold mt-3 mb-4">
-           For Summer Sales
-          </h1>
-          <p className="text-lg text-gray-600 font-semibold">
-            New Product Purchase up to 50% Off All Now Fast
-          </p>
-          <p className="text-lg text-gray-600 font-semibold">Shopping</p>
-          <button className="button-style relative overflow-hidden upparcase bg-[#2563eb] text-white hover:text-[#2563eb] hover:bg-white border border-[#2563eb] px-4 py-1.5 font-semibold rounded-md mt-5 duration-300 z-0">
-            Shop Now
-          </button>
+    <div>
+      <div className="pb-12 lg:px-0 px-4 relative promo-background">
+        <div className="absolute w-full top-[50%] -translate-y-[50%]">
+          <div className="w-full flex justify-between items-center gap-x-4 container mx-auto">
+            <div></div>
+            <div className="uppercase lg:block hidden text-left w-1/2">
+              <h3 className="text-3xl text-[#2563eb] font-bold">
+                Sales Offer & Discount Only
+              </h3>
+              <h1 className="text-6xl upparcase font-bold mt-3 mb-4">
+                For Summer Sales
+              </h1>
+              <p className="text-lg text-gray-600 font-semibold">
+                New Product Purchase up to 50% Off All Now Fast
+              </p>
+              <p className="text-lg text-gray-600 font-semibold">Shopping</p>
+
+              <div className="my-5 lg:px-0 md:px-0 px-4 w-1/2">
+                <div className="flex gap-x-2 items-center">
+                  <div className="w-full px-5 py-3 border rounded-md text-center shadow-md border-b-[6px] border-b-[#2563eb]">
+                    <p className="text-4xl font-bold text-orange-600">
+                      {timerDays}
+                    </p>
+                    <p className="mt-1 font-semibold">Days</p>
+                  </div>
+                  <div className="w-full px-5 py-3 border rounded-md text-center shadow-md border-b-[6px] border-b-[#2563eb]">
+                    <p className="text-4xl font-bold text-orange-600">
+                      {timerHours}
+                    </p>
+                    <p className="mt-1 font-semibold">Hrs</p>
+                  </div>
+                  <div className="w-full px-5 py-3 border rounded-md text-center shadow-md border-b-[6px] border-b-[#2563eb]">
+                    <p className="text-4xl font-bold text-orange-600">
+                      {timerMinutes}
+                    </p>
+                    <p className="mt-1 font-semibold">Mins</p>
+                  </div>
+                  <div className="w-full px-5 py-3 border rounded-md text-center shadow-md border-b-[6px] border-b-[#2563eb]">
+                    <p className="text-4xl font-bold text-orange-600">
+                      {timerSeconds}
+                    </p>
+                    <p className="mt-1 font-semibold">Secs</p>
+                  </div>
+                </div>
+              </div>
+              <a
+                href="/shopCategory/All"
+                className="button-style relative overflow-hidden upparcase bg-[#2563eb] text-white hover:text-[#2563eb] hover:bg-white border border-[#2563eb] px-4 py-1.5 font-semibold rounded-md mt-5 duration-300 z-0 inline-block"
+              >
+                Shop Now
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="container mx-auto">
+      <div className="container mx-auto mt-5">
         <div className="lg:flex block justify-between gap-x-6 items-center">
           <Link to="/shopCategory/gloves" className="block w-full">
             <div className="lg:py-6 md:py-5 py-3 lg:px-6 md:px-5 px-1 flex justify-between items-center border rounded-md hover:shadow-md duration-300 w-full lg:my-0 my-2">
               <img src={care1} alt="Care_Image" />
               <div className="text-left pl-3">
-                <h4 className="text-xl font-bold mb-2">Covid Essentials</h4>
+                <h4 className="text-xl font-bold mb-2">Women Styles</h4>
                 <p className="my-1 hover:text-[#2563eb] duration-300 cursor-pointer">
-                  Covid Essentials
+                  Trending Fashion
                 </p>
                 <p className="my-1 hover:text-[#2563eb] duration-300 cursor-pointer">
                   Disinfectants
                 </p>
                 <p className="my-1 hover:text-[#2563eb] duration-300 cursor-pointer">
-                  Home Hygiene Essentials
+                  Home Style Dress Pack
                 </p>
                 <p className="my-1 hover:text-[#2563eb] duration-300 cursor-pointer">
-                  Masks
+                  Long Dress
                 </p>
                 <button className="text-md font-semibold text-[#2563eb] hover:underline duration-300 flex items-center gap-x-2 mt-5">
-                  Biew all Covid Essentials
+                  View all Fshions
                   <BsArrowRight />
                 </button>
               </div>
@@ -64,21 +129,21 @@ const PromoBannar = () => {
             <div className="lg:py-6 md:py-5 py-3 lg:px-6 md:px-5 px-1 flex justify-between items-center border rounded-md hover:shadow-md duration-300 w-full lg:my-0 my-2">
               <img src={care2} alt="Care_Image" />
               <div className="text-left pl-3">
-                <h4 className="text-xl font-bold mb-2">Covid Essentials</h4>
+                <h4 className="text-xl font-bold mb-2">Mens Fashions</h4>
                 <p className="my-1 hover:text-[#2563eb] duration-300 cursor-pointer">
-                  Covid Essentials
+                  Mens Trending Style
                 </p>
                 <p className="my-1 hover:text-[#2563eb] duration-300 cursor-pointer">
                   Disinfectants
                 </p>
                 <p className="my-1 hover:text-[#2563eb] duration-300 cursor-pointer">
-                  Home Hygiene Essentials
+                  Mens Home dress Up
                 </p>
                 <p className="my-1 hover:text-[#2563eb] duration-300 cursor-pointer">
-                  Masks
+                 Essential Items
                 </p>
                 <button className="text-md font-semibold text-[#2563eb] hover:underline duration-300 flex items-center gap-x-2 mt-5">
-                  Biew all Covid Essentials
+                  View All Fashions
                   <BsArrowRight />
                 </button>
               </div>
@@ -89,21 +154,21 @@ const PromoBannar = () => {
             <div className="lg:py-6 md:py-5 py-3 lg:px-6 md:px-5 px-1 flex justify-between items-center border rounded-md hover:shadow-md duration-300 w-full lg:my-0 my-2">
               <img src={care3} alt="Care_Image" />
               <div className="text-left pl-3">
-                <h4 className="text-xl font-bold mb-2">Covid Essentials</h4>
+                <h4 className="text-xl font-bold mb-2">Kids Fashions</h4>
                 <p className="my-1 hover:text-[#2563eb] duration-300 cursor-pointer">
-                  Covid Essentials
+                 Kids Style Items
                 </p>
                 <p className="my-1 hover:text-[#2563eb] duration-300 cursor-pointer">
                   Disinfectants
                 </p>
                 <p className="my-1 hover:text-[#2563eb] duration-300 cursor-pointer">
-                  Home Hygiene Essentials
+                  Kids Trending Items
                 </p>
                 <p className="my-1 hover:text-[#2563eb] duration-300 cursor-pointer">
                   Masks
                 </p>
                 <button className="text-md font-semibold text-[#2563eb] hover:underline duration-300 flex items-center gap-x-2 mt-5">
-                  Biew all Covid Essentials
+                  View Kids Items
                   <BsArrowRight />
                 </button>
               </div>
